1b1c724db2daa6b99ecfdeac7bf3503ceb981392
alewilliam789 <alewilliam789@gmail.com>
2023-04-29 13:10:58 -0700

    Add functionality to the backspace button

--- source/components/Calculator.js
+++ source/components/Calculator.js
@@ -15,18 +15,30 @@ export default function Calculator(props){
         console.log("Calculated the total")
     }
 
+    function handleBack(){
+        setScreenText( prevScreenText => {
+            let previousValue = prevScreenText.slice(-1);
+            let length = prevScreenText.length
+            return (operators.includes(previousValue) ? prevScreenText.slice(0,length-2) : prevScreenText.slice(0,length-1))
+        })
+    }
+
     function handleClear(){
         setScreenText("")
     }
 
-    const nums = [...Array(10).keys(), "CLR"];
+    const nums = [...Array(10).keys()];
+    const erasors = ["BCK","CLR"];
     const operators = ["+", "-","/","*","^","="];
     const [screenText, setScreenText] = useState("")
 
     const numberButtons = nums.map(num => {
-        return num =="CLR" ? <Button key={num} value={num} handleClick={handleClear}/> : <Button key={num} value={num} handleClick={handleClick}/>
+       return <Button key={num} value={num} handleClick={handleClick}/>
     })
 
+    const erasorButtons = erasors.map(num => {
+        return num =="CLR" ? <Button key={num} value={num} handleClick={handleClear}/> : <Button key={num} value={num} handleClick={handleBack}/>
+    })
 
     const operatorButtons = operators.map((operator, index) =>{
         return operator == "=" ? <Button key={index} value={operator} handleClick={handleCalc}/> : <Button key={index} value={operator} handleClick={handleClick}/>
@@ -42,6 +54,7 @@ export default function Calculator(props){
             <div className="mt-20 gap-10 flex">
                 <div className="w-70 grid grid-cols-3 gap-5">
                     {numberButtons}
+                    {erasorButtons}
                 </div>
                 <div className="flex flex-col justify-center gap-5">
                     {operatorButtons}
